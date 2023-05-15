5f40e5d9eb62ce78cd54382053cb75e29c4a933a
Jonny Watson <PixMusix@gmail.com>
2023-05-16 00:46:28 +1000

    Reformatted. Added LedStrip.get(). Error no matching function for call to CFastLED::addLeds(CRGB [8], int&)

--- Led.h
+++ Led.h
@@ -1,20 +1,24 @@
+#include <FastLED.h>
+
 class Led {
 
-  protected:
-
-    byte pin;
-    int numLed;
+  private:
 
     void init() {
       pinMode(pin, OUTPUT);
       off();
     }
 
+  protected:
+
+    byte pin;
+    int size;
+
   public:
 
     Led(byte pin) {
       this->pin = pin;
-      numLed = 1;
+      size = 1;
       init();
     }
 
@@ -28,27 +32,76 @@ class Led {
 
 };
 
-// class LedMatrix : public Led {
+class LedStrip : public Led {
 
-//   private:
-//     void ledPop() {
-//       FastLED.show();
-//       delay(20);
-//     }
+  private:
 
-//     init() {
+    CRGB matrix[8];
 
-//     }
+  protected:
+    void init() {
+      pinMode(pin, OUTPUT);
+      FastLED.addLeds<WS2812B, pin, GRB>(matrix, size); 
+    }
 
-//   public:
+    void ledPop() {
+      FastLED.show();
+    }
 
-//     LedStrip(byte pin, int count) : Led { pin, count } {
-//       CGRB leds[NUM_LEDS];
-//       this->pin = pin;
-//       numLed = count;
-//       FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, numLed); 
-//       matrix = 
-//       init();
-//     }
+  public:
 
-// }
+    LedStrip(byte pin) : Led { pin } {     
+      this->pin = pin;
+      size = 8;
+      init();
+    }
+
+    CRGB get(int i) {
+      if (i < size) {
+        return matrix[i];
+      } else {
+        return matrix[0];
+      }
+    }
+
+};
+
+class LedRainbow : public LedStrip {
+
+  private:
+
+    CRGB matrix[13];
+
+  public:
+
+    LedRainbow(byte pin) : LedStrip { pin } {
+      size = 13;
+      this->pin = pin;
+      init();
+    }
+
+};
+
+class LedMatrix : public LedStrip {
+
+  private:
+
+    CRGB matrix[64];
+
+  public:
+
+    LedMatrix(byte pin) : LedStrip { pin } {     
+      this->pin = pin;
+      size = 64;
+      init());
+    }
+
+    CRGB get(int x, int y) {
+      int i = x + y * 8;
+      if (i < size) {
+        return matrix[i];
+      } else {
+        return matrix[0];
+      }
+    }
+};
